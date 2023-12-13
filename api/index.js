fb60b3c5f9f1722ef17b5ac9d28142e3554d54d3
Apoorv <awasthiapoorv23@gmail.com>
2023-12-13 11:39:09 +0530

    sign up page functionality complete

--- api/index.js
+++ api/index.js
@@ -7,30 +7,31 @@ import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 const app = express();
-mongoose.connect(process.env.MONGO)
-    .then(() => {
-        console.log("Connected to MongoDB");
-    })
-    .catch((error) => {
-        console.log(error);
-    });
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 app.use(express.json());
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000");
 });
 
-app.use('/api/user', userRouter);
+app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || "Internal Server Error";
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
 
-    return res.status(statusCode).json({
-        "success": false,
-        "statusCode": statusCode,
-        "message": message
-    });
+  return res.status(statusCode).json({
+    success: false,
+    statusCode: statusCode,
+    message: message,
+  });
 });
